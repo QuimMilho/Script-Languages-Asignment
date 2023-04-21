d3572af2662e4ba0bd4fb98ab01759b00c877661
Quim Milho <jrodmilheiro@gmail.com>
2023-04-21 18:04:42 +0100

    Adicionado numero de aluno do Caeta

--- site/src/components/page/Footer.tsx
+++ site/src/components/page/Footer.tsx
@@ -7,7 +7,7 @@ export default function (props: {}) {
                 <ul>
                     <li className="white bold centered">Desenvolvido por:</li>
                     <li className="white centered parent">Joaquim Milheiro - 2020131794</li>
-                    <li className="white centered parent">Lucas Caetano - 202???????</li>
+                    <li className="white centered parent">Lucas Caetano - 2020132564</li>
                     <li className="white centered parent">João Pinheiro - 2020131761</li>
                 </ul>
             </div>
